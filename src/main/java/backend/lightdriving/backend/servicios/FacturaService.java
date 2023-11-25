2e8565a9b5f3603a7e1a3806cdbb9e13d8bea201
Dorian Samantha Contreras <doriansamanthacontreras@gmail.com>
2023-11-25 04:32:42 -0600

    Quitar algunos servicios de algunos modelos.

--- src/main/java/backend/lightdriving/backend/servicios/FacturaService.java
+++ src/main/java/backend/lightdriving/backend/servicios/FacturaService.java
@@ -4,11 +4,8 @@ import backend.lightdriving.backend.modelos.Factura;
 
 public interface FacturaService {
 
-    public Factura crearFactura(Factura Factura);
-
-    public boolean eliminarFactura(int idFactura);
-
     public boolean actualizarFactura(int idFactura, Factura Factura);
     
     public Factura obtenerFactura(int idFactura);
+
 }
