a7abf681a461e6978fba5010cffb6e0395b2c8a7
Dorian Samantha Contreras <doriansamanthacontreras@gmail.com>
2023-11-29 17:16:21 -0600

    Depuracion Metodo pago

--- src/main/java/backend/lightdriving/backend/servicios/Implementaciones/MetodoPagoServiceImpl.java
+++ src/main/java/backend/lightdriving/backend/servicios/Implementaciones/MetodoPagoServiceImpl.java
@@ -16,48 +16,6 @@ public class MetodoPagoServiceImpl implements MetodoPagoService{
     private MetodoPagoRepository metodoPagoRepository;
 
     @Override
-    public boolean crearMetodoPago(MetodoPago metodoPago) {
-        if(metodoPago != null){
-            this.metodoPagoRepository.save(metodoPago);
-            return true;
-        }
-
-        return false;
-    }
-
-    @Override
-    public boolean eliminarMetodoPago(int idMetodoPago) {
-        
-        if(this.metodoPagoRepository.existsById(idMetodoPago)){
-            this.metodoPagoRepository.deleteById(idMetodoPago);
-            return true;
-        }
-
-        return false;
-    }
-
-    @Override
-    public boolean actualizarMetodoPago(int idMetodoPago, MetodoPago metodoPago) {
-        if(this.metodoPagoRepository.existsById(idMetodoPago)){
-            MetodoPago metodoPago2= this.metodoPagoRepository.findById(idMetodoPago).get();
-            metodoPago2.setDescripcion(metodoPago.getDescripcion());
-            return true;
-        }
-
-        return false;
-    }
-
-    @Override
-    public MetodoPago obtenerMetodoPago(int idMetodoPago) {
-        
-        if(this.metodoPagoRepository.existsById(idMetodoPago)){
-            return this.metodoPagoRepository.findById(idMetodoPago).get();
-        }
-
-        return null;
-    }
-
-    @Override
     public List<MetodoPago> obtenerMetodos() {
         return this.metodoPagoRepository.findAll();
     }
