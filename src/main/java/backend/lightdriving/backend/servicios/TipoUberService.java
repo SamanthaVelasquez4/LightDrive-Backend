b272fca47c65dc3bda73aaa0ee2b670167d4885b
Dorian Samantha Contreras <doriansamanthacontreras@gmail.com>
2023-11-29 17:13:43 -0600

    Depuracion Uber

--- src/main/java/backend/lightdriving/backend/servicios/TipoUberService.java
+++ src/main/java/backend/lightdriving/backend/servicios/TipoUberService.java
@@ -3,12 +3,6 @@ package backend.lightdriving.backend.servicios;
 import backend.lightdriving.backend.modelos.TipoUber;
 
 public interface TipoUberService {
-
-    public boolean crearTipoUber(TipoUber TipoUber);
-
-    public boolean eliminarTipoUber(int idTipoUber);
-
-    public boolean actualizarTipoUber(int idTipoUber, TipoUber TipoUber);
     
     public TipoUber obtenerTipoUber(int idTipoUber);
 }
